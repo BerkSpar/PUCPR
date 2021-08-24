b587e8cff03c93c55690609865b9edf0f1ae0d74
Felipe <felipe@lftecnologia.com.br>
2021-08-24 20:19:28 -0300

    fix collatz conjecture

--- OOP/collatz_conjucture/src/Collatz.java
+++ OOP/collatz_conjucture/src/Collatz.java
@@ -6,26 +6,30 @@
 import java.util.Scanner;
 
 public class Collatz {
+    static void collatz(int number) {
+        System.out.println(number);
+        while (number != 1) {
+            if (number % 2 == 0) {
+                number = number / 2;
+            } else {
+                number = 3 * number + 1;
+            }
+
+            System.out.println(number);
+        }
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
         System.out.print("Please type a positive integer number: ");
         int inputData = in.nextInt();
 
-        if(inputData < 1) {
+        if (inputData < 1) {
             System.out.println("Invalid number");
             return;
         }
 
-        int currentNumber = inputData;
-        while (currentNumber != 1) {
-            if(currentNumber%2 == 0) {
-                currentNumber = currentNumber/2;
-            } else {
-                currentNumber = 3 * currentNumber + 1;
-            }
-
-            System.out.println(currentNumber);
-        }
+        collatz(inputData);
     }
 }
